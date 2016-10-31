8512a8bf389ec1e83bdaf6fdc4d21a8bd61d0ea3
kopringo <kopringo@gmail.com>
2016-10-31 01:40:40 +0100

    dfs, data structure

--- g2t.py
+++ g2t.py
@@ -10,25 +10,31 @@ class NodePerson:
     parents = None
     families = []
 
+    v_width = 1
+
     def get_father(self):
-        pass
+        return self.father
 
     def get_mother(self):
-        pass
+        return self.mother
 
     def get_families(self):
-        pass
+        return self.families
 
 class NodeFamily:
 
+    father = None
+    mother = None
+    children = []
+
     def get_father(self):
-        pass
+        return self.father
 
     def get_mother(self):
-        pass
+        return self.mother
 
     def get_children(self):
-        pass
+        return self.children
 
 class Gedcom2Tree:
     """
@@ -45,19 +51,29 @@ class Gedcom2Tree:
         pass
 
     def load_gedcom(self, file_path):
-        pass
+
+        # @todo
+
+        self.build_tree()
 
     def load_raw_data(self, person_list, family_list):
-        for person in person_list:
-            self.person_list[person['id']] = person
-            self.person_list[person['id']]['v'] = {
-                'vx': None, # virtual x
-                'vy': None, # virtual y
-                'v_families': None,
+        for person_id in person_list.keys():
+            self.person_list[person_id] = NodePerson()
 
-            }
-        for family in family_list:
-            self.family_list[family['id']] = family
+        for family_id in family_list:
+            self.family_list[family_id] = NodeFamily()
+
+            self.family_list[family_id].father = self.person_list[ family_list[family_id]['father_id'] ]
+            self.family_list[family_id].mother = self.person_list[ family_list[family_id]['mother_id'] ]
+            for children_id in family_list[family_id]['children_ids']:
+                self.family_list[family_id].children.append( self.person_list[children_id] )
+
+            if family_list[family_id]['father_id'] != None:
+                self.person_list[ family_list[family_id]['father_id'] ].families.append( self.family_list[family_id] )
+            if family_list[family_id]['mother_id'] != None:
+                self.person_list[ family_list[family_id]['mother_id'] ].families.append( self.family_list[family_id] )
+
+        self.build_tree()
 
 
     def clear_dfs(self):
@@ -71,3 +87,49 @@ class Gedcom2Tree:
     def build_tree(self):
         pass
 
+
+    def _calc_tree(self, person):
+
+        families = person.get_families()
+
+        if families == []:
+            person.v_width = 1
+            return 1
+
+        sum_all = 0
+
+        # add spaces between families
+        if len(families) > 0:
+            sum_all = sum_all + len(families) - 1
+
+
+        for family in families:
+            children = family.get_children()
+            v_sum = 0
+
+            # add spaces between children
+            if len(children) > 0:
+                v_sum = v_sum + len(children) - 1
+
+            # calculate width of child
+            for child in children:
+                self._calc_tree(child)
+                v_sum = v_sum + child.v_width
+
+            family.v_width = v_sum
+
+            sum_all = sum_all + v_sum
+
+        person.v_width = sum_all
+        return sum_all
+
+    def gen_tree(self, person_id):
+
+        if not person_id in self.person_list.keys():
+            raise 'person not found'
+
+        person = self.person_list[person_id]
+        while person.parents != None and person.parents.father != None:
+            person = person.family.father
+
+        self._calc_tree(person)
--- test.py
+++ test.py
@@ -2,3 +2,47 @@
 
 from g2t import Gedcom2Tree
 
+person_list = {
+    1: {},
+    2: {},
+    3: {},
+    4: {},
+    5: {},
+    6: {},
+    7: {},
+    8: {},
+    9: {},
+    10: {},
+    11: {},
+    12: {},
+    13: {},
+    14: {},
+    15: {},
+    16: {},
+    17: {},
+    18: {},
+    19: {},
+    20: {},
+    21: {},
+    22: {},
+    23: {},
+    24: {},
+    25: {},
+    26: {},
+    27: {},
+    28: {},
+}
+
+family_list = {
+    1: {'father_id': 1, 'mother_id': 2, 'children_ids': [3, 4]},
+    2: {'father_id': 6, 'mother_id': 7, 'children_ids': [23, 24, 25, 26, 27]},
+    3: {'father_id': 26, 'mother_id': 27, 'children_ids': [28]},
+    4: {'father_id': 10, 'mother_id': 9, 'children_ids': [11, 12, 13]},
+    5: {'father_id': 14, 'mother_id': 15, 'children_ids': [9, 8, 1, 2, 5, 6]},
+    6: {'father_id': 10, 'mother_id': 20, 'children_ids': [14, 15, 16, 17, 18]},
+    7: {'father_id': 21, 'mother_id': 22, 'children_ids': [19]},
+}
+
+g = Gedcom2Tree()
+g.load_raw_data(person_list, family_list)
+g.gen_tree(26)
